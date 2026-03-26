4c55c79faa3c7b27e0e4b8cdd7266017664653ba
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2026-03-25 22:07:35 -0500

    CF modificado

--- src/config/global.js
+++ src/config/global.js
@@ -113,7 +113,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/123101_CF13_DU.pdf',
+        download: 'downloads/133100_CF13_DU.pdf',
       },
       {
         icono: 'fas fa-download',
